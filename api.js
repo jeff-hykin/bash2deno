547336cfc12c578e5c72fad7104e3ce10ac84542
Jeff Hykin <jeff.hykin@gmail.com>
2025-11-07 14:48:12 -0600

    -

--- api.js
+++ api.js
@@ -680,7 +680,7 @@ export function translate(code, { withHeader=true }={}) {
                     `import * as dax from "https://esm.sh/@jsr/david__dax@0.43.2/mod.ts" // see: https://github.com/dsherret/dax`,
                     `import * as path from "https://esm.sh/jsr/@std/path@1.1.2"`,
                     `import { env, aliases, $stdout, $stderr, initHelpers, iterateOver } from "https://esm.sh/gh/jeff-hykin/bash2deno@0.1.0.2/helpers.js"`,
-                    `let { $, appendTo, overwrite, hasCommand, makeScope, settings, exitCodeOfLastChildProcess } = initHelpers({ dax })`,
+                    `let { $, appendTo, overwrite, hasCommand, makeScope, settings, exitCodeOfLastChildProcess } = initHelpers({ dax })\n`,
                 ]
                 return header.join("\n")+"\n"+contents
             } else if (node.type == "$(" || node.type == ")" || node.type == "`") {
